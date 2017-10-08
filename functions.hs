bc6e9922f637e21e3cc98e9890ee03ecafc28e6a
Michael Mejia <michael.mejia246@gmail.com>
2017-10-08 21:41:15 +0200

    add

---
+++ functions.hs
@@ -0,0 +1,32 @@
+import Data.List
+import System.IO
+
+addMe :: Int -> Int -> Int
+
+-- funcName param1 param2 = operation (returned value)
+
+addMe x y = x + y
+
+sumMe x y = x + y --Didnt define the fuction so it works with floats
+
+addTuples :: (Int, Int) -> (Int, Int) -> (Int, Int)
+
+addTuples (x, y)(x2, y2) = (x + x2, y + y2)
+whatAge :: Int -> String
+whatAge 16 = "You can drive"
+whatAge 18 = "you can vote"
+whatAge 21 = "You're an adult"
+whatAge _ = "nothing important"
+
+--recursion 
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial(n-1)
+
+prodFact n = product [1..n]
+
+isOdd :: Int -> Bool
+isOdd n
+	| n `mod` 2 == 0 = False
+	| otherwise = True
--- newtype.hs
+++ newtype.hs
@@ -6,4 +6,21 @@ maxInt = maxBound :: Int
 
 num = min 9 12
 num2 = max 11 14
-let x = snd (10,14) in x
+sumOfNums = sum [1..1000]
+modEx = mod 5 4
+
+num9 = 9 :: Int
+sqrtOf9 = sqrt(fromIntegral num9)
+
+trueandfalse = True && False
+trueorflase = True || False
+nottrue = not(True)
+primeNumbers = [3,5,7,11]
+morePrime = primeNumbers ++ [13,17,19,23,29]
+favNums = 2 : 7 : 21 :66 :[]
+multilist = [[1,2,3],[4,5,6],[7,9,0]]
+moreprime2 = 31 : morePrime 
+lengthPrime = length moreprime2 --length of List
+revPrime = reverse moreprime2 -- shows list in reverse order
+firstprime = head moreprime2 -- first in list
+lasprime = last moreprime2 -- last in list
